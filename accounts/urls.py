d2f3acd046c514c7aa1e20e0082aa128dc86e202
shubham <sps371997@gmail.com>
2020-10-06 01:24:50 +0530

    setup basic structure

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from accounts.views import Signup, Login,  ForgotPassword, ResetPassword
+
+urlpatterns = [
+    url(r'^signup/', Signup.as_view(), name='signup_student'),
+    url(r'^login/', Login.as_view(), name='login_student'),
+    url(r'^forgot_password/', ForgotPassword.as_view(), name='forgot_password_link'),
+    url(r'^reset_password/', ResetPassword.as_view(), name='reset_password'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from rest_framework.views import  APIView
+from rest_framework import status 
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from  django.contrib.auth.models import  User
 
-# Create your views here.
+
+class Signup(APIView):
+
+    permission_classes = [AllowAny,]
+
+    def post(self, request, format=None):
+        """
+            Api for Create Student.
+        """
+        import ipdb; ipdb.set_trace()
+        return Response(data=None, status=status.HTTP_201_CREATED)
+
+
+class Login(APIView):
+
+    permission_classes = [AllowAny,]
+
+    def get(self, request, format=None):
+        """
+            Api for LogIn Student.
+        """
+        return Response(data=None, status=status.HTTP_200_OK)
+
+
+class ForgotPassword(APIView):
+
+    permission_classes = [AllowAny,]
+
+    def post(self, request, format=None):
+        """
+            Api for Sent Reset Password Link To User Email.
+        """
+        return Response(data=None, status=status.HTTP_200_OK)
+
+
+class ResetPassword(APIView):
+
+    permission_classes = [AllowAny,]
+
+    def post(self, request, format=None):
+        """
+            Api for Reset Password.
+        """
+        return Response(data=None, status=status.HTTP_205_RESET_CONTENT)
